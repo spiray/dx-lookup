e9ba2d8eae308a6585c6c004e3dfec00f7656912
spiray <spira.joe214@gmail.com>
2019-12-02 12:45:46 -0500

    added ggh-pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Container, Navbar, Form, Card } from 'react-bootstrap';
+import { acuteCodes, chronicCodes } from './codes';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [result, setResult] = useState('Enter search term above to find the Dx type...');
+
+  function handleInputChange({ target: { value } }) {
+    setSearchTerm(value);
+  }
+
+  function setAndSearch(event) {
+    event.preventDefault();
+    if (searchTerm in acuteCodes) setResult('Acute');
+    else if (searchTerm in chronicCodes) setResult('Chronic');
+    else setResult('Diagnosis not found...');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#">Dx Lookup</Navbar.Brand>
+      </Navbar>
+      <Container className="App">
+        <Form className="mt-3" onSubmit={setAndSearch}>
+          <Form.Group>
+            <Form.Control type="text" placeholder="Enter Diagnosis Code..." onChange={handleInputChange}></Form.Control>
+          </Form.Group>
+        </Form>
+        <Card>
+          <Card.Body>
+            <Card.Title>{result}</Card.Title>
+          </Card.Body>
+        </Card>
+      </Container>
     </div>
   );
 }
